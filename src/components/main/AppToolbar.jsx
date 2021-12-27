0308ba75541a09d36a0d2de37ee1e939af6a4078
jacob-alspaw <jacob.alspaw@hcl.com>
2021-12-27 14:06:43 -0600

    Update page logo

--- src/components/main/AppToolbar.jsx
+++ src/components/main/AppToolbar.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import logoIconSrc from '../../assets/images/logoIcon.svg';
+import logoTextSrc from '../../assets/images/logoText.png';
 import { paths } from '../../constants';
 import defineBlock from '../../utils/defineBlock';
 import './AppToolbar.scss';
@@ -15,12 +17,24 @@ const AppToolbar = () => (
     sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
   >
     <Toolbar>
-      <Link
-        className={bem('link')}
-        to={paths.HOME}
-      >
-        Game Breaker
-      </Link>
+      <div className={bem('button')}>
+        <Link
+          className={bem('link')}
+          to={paths.HOME}
+        >
+          <img
+            className={bem('icon')}
+            src={logoIconSrc}
+            alt="Game Break Icon"
+            height="40"
+          />
+          <img
+            src={logoTextSrc}
+            alt="Game Break Text"
+            height="40"
+          />
+        </Link>
+      </div>
     </Toolbar>
   </AppBar>
 );
