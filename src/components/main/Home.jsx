274a71f1c61dcd41ba51ed0db383d7a7f96704b9
jacob-alspaw <jacob.alspaw@hcl.com>
2021-12-28 23:59:22 -0600

    Update home page

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
 import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
 import logoIconSrc from '../../assets/images/logoIcon.svg';
 import logoTextSrc from '../../assets/images/logoText.png';
@@ -27,21 +30,42 @@ const Home = () => (
         />
       </div>
       <Typography className={bem('subheader')} variant="h5" component="div">
-        We help you do something something something. Come use our
-        site because we are #1 in helping you do that thing!
+        An advanced application that strategizes your next
+        move for common games. Our algorithmic approach to
+        game theory is gauranteed to keep you winning!
       </Typography>
     </Box>
     <PageBase>
       <PageTitle text="About" />
-      <Typography variant="body1" component="div">
-        TODO: how we find our solutions (consulting community/academic papers)
+      <Typography className={bem('description')} variant="body1" gutterBottom component="div">
+        We curate our solutions to games using algorithms that are gauranteed to help
+        you win. Since the  1950s, academia has extensively studied and improved upon
+        game theory — mathematical models of strategic interactions used in analyzing
+        and dealing with competitive situations or games. A game&apos;s outcome is
+        critically dependent on it&apos;s players&apos; choice of actions. Our service
+        helps you make the best possible choices to maximize the chance for favorable
+        outcomes in your games.
+      </Typography>
+      <Typography className={bem('description')} variant="body1" component="div">
+        Our algorithms are gathered directly from the community through message
+        boards, scholarly articles, and more. We can ensure that our clients recieve
+        the best possible service by handpicking community solutions that have been
+        tested and approved by many. Game Breaker exclusively provides client-side
+        processing solutions, so it is necessary that our algorithms remain performant
+        considering some clients&apos; limited access to resources like computer memory
+        and processing power.
+      </Typography>
+      <Typography className={bem('description')} variant="body1" component="div">
+        Try out our game breaking service and improve your game today!
       </Typography>
     </PageBase>
+    <Divider />
     <PageBase>
       <PageTitle text="Features" />
-      <Typography variant="body1" component="div">
-        TODO: add some features
-      </Typography>
+      <Alert className={bem('no-results')} severity="info">
+        <AlertTitle>In progress</AlertTitle>
+        We are still working on many features. Bear with us as we get things up and running!
+      </Alert>
     </PageBase>
   </div>
 );
